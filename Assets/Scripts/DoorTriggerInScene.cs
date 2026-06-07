4f5fd9b254e605f6adf1138ef949e06956e1afb4
Rae Ralston <raeralston@pm.me>
2026-06-07 08:29:47 -0700

    add interact system

--- Assets/Scripts/DoorTriggerInScene.cs
+++ Assets/Scripts/DoorTriggerInScene.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
 using Unity.Cinemachine;
 
-public class DoorTriggerInScene : MonoBehaviour
+public class DoorTriggerInScene : MonoBehaviour, IInteractable
 {
   [SerializeField] private Transform destination;
+  private Transform player;
+
+  public void Interact()
+  {
+
+    Vector3 delta = destination.position - player.position;
+    player.position = destination.position;
+    CinemachineCore.OnTargetObjectWarped(player, delta);
+  }
 
   private void OnTriggerEnter2D(Collider2D other)
   {
     if (other.CompareTag("Player"))
     {
-      Vector3 delta = destination.position - other.transform.position;
-      other.transform.position = destination.position;
-      CinemachineCore.OnTargetObjectWarped(other.transform, delta);
+      player = other.transform;
+      other.GetComponent<PlayerMovement>().SetInteractable(this);
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D other)
+  {
+    if (other.CompareTag("Player"))
+    {
+      player = null;
+      other.GetComponent<PlayerMovement>().ClearInteractable();
     }
   }
 }
