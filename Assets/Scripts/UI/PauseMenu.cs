cc052a556b539eadcd4bdb842093c6831eded515
DireBlackAudio <direblackaudio@gmail.com>
2026-06-08 18:19:09 +0200

    audio added to pause menu
    When pausing the game, the current game sound is muted (atmo, player sounds)
    and a dedicated audio for the pause menu is played/unmuted

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,17 +1,47 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using FMODUnity;
+using FMOD.Studio;
 
 public class PauseMenu : MonoBehaviour
 {
   [SerializeField] private GameObject menuPanel;
   [SerializeField] private GameObject controlsPanel;
   [SerializeField] private PlayerMovement playerMovement;
+  [SerializeField] private EventReference FMODMenuEvent;
+  [SerializeField] private EventReference FMODMenuSnapshot;  
+  private EventInstance menuAudio;
+  private EventInstance menuSnapshot;
   private bool isPaused = false;
 
   private void Start()
   {
     menuPanel.SetActive(false);
     controlsPanel.SetActive(false);
+    StartMenuAudio();
+  }
+
+  /*
+   * FMOD: Start Audio playback muted/in background
+   * This way the audio does not restart everytime the player presses pause. 
+   * If it turns out to be too processing heavy, we can move this call into TogglePause(), 
+   * just before calling the snapshot
+   */
+  private void StartMenuAudio() 
+  {
+    menuAudio = RuntimeManager.CreateInstance(FMODMenuEvent);
+    menuAudio.start();
+  }
+
+  /*
+   * FMOD: activate audio mixer snapshot
+   * laods a snaptshot of the FMOD audio mixer in which the current game audio (atmo, player sounds) is muted 
+   * and pause menu sounds/music are unmuted
+   */
+  private void activateSnapshot()
+  {
+    menuSnapshot = RuntimeManager.CreateInstance(FMODMenuSnapshot);
+    menuSnapshot.start();
   }
 
   private void Update()
@@ -30,6 +60,7 @@ public class PauseMenu : MonoBehaviour
       menuPanel.SetActive(true);
       controlsPanel.SetActive(false);
       Time.timeScale = 0f;
+      activateSnapshot(); // call FMOD Snapshot      
     }
     else
     {
@@ -37,7 +68,8 @@ public class PauseMenu : MonoBehaviour
       menuPanel.SetActive(false);
       controlsPanel.SetActive(false);
       Time.timeScale = 1f;
-    }
+      if (menuSnapshot.isValid()) menuSnapshot.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+      }
   }
 
   public void OnResumePressed()
